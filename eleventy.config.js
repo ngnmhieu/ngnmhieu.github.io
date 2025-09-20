e7b23f1db2044fb67553c53000510dea31f2b105
Hieu Nguyen <ngnmhieu@gmail.com>
2025-09-20 15:04:34 +0200

    use 11ty to generate

---
+++ eleventy.config.js
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+import cssnano from 'cssnano';
+import postcss from 'postcss';
+import tailwindcss from '@tailwindcss/postcss';
+
+const SOURCE_DIR = 'src';
+const OUTPUT_DIR = '_site';
+
+export default function (eleventyConfig) {
+
+  eleventyConfig.on('eleventy.before', async () => {
+    const tailwindInputPath = path.resolve(`./${SOURCE_DIR}/assets/input.css`);
+    const tailwindOutputPath = `./${OUTPUT_DIR}/assets/output.css`;
+    const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
+    const outputDir = path.dirname(tailwindOutputPath);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const result = await processor.process(cssContent, {
+      from: tailwindInputPath,
+      to: tailwindOutputPath,
+    });
+
+    fs.writeFileSync(tailwindOutputPath, result.css);
+  });
+
+  const processor = postcss([
+    //compile tailwind
+    tailwindcss(),
+
+    //minify tailwind css
+    cssnano({
+      preset: 'default',
+    }),
+  ]);
+
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: 'src',
+      output: '_site'
+    }
+  };
+};
